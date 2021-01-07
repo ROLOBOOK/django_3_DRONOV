e79d5f09888749c84ea0afc0fb10e8fe2473d05e
rolobook <rolobook@yandex.ru>
2021-01-07 20:12:32 +0300

    use django.shortcuts import render in bboard.view

--- bboard/views.py
+++ bboard/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+
 
 from .models import Bd
 
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bd.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
 
