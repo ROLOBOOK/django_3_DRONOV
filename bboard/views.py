d1b87bac0e702a906112016dd31c509db7ea7da4
rolobook <rolobook@yandex.ru>
2021-01-07 19:44:57 +0300

    update bd.views.index return HttpResponse(Bd.objects.order_by('-published'))

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bd
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений')
+    s = 'Список объявлений \r\n\r\n\r\n'
+    for b in Bd.objects.order_by('-published'):
+        s += f'{b.title} \r\n {b.content} \r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
