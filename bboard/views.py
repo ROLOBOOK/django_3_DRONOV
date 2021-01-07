6bb849ca38fd2739430eab728462e964b59eb427
rolobook <rolobook@yandex.ru>
2021-01-07 20:10:30 +0300

    move index.html in bboard/templates/bboard/. update bboardviews.py

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Bd
 
 
 # Create your views here.
 def index(request):
-    s = 'Список объявлений \r\n\r\n\r\n'
-    for b in Bd.objects.order_by('-published'):
-        s += f'{b.title} \r\n {b.content} \r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
+
