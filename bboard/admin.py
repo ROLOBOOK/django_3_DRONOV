04f09ec1e2e4ce178855f1a42d61fe68fdd78c56
rolobook <rolobook@yandex.ru>
2021-01-08 22:08:15 +0300

    add  class BdAdmib in bboard.admin

--- bboard/admin.py
+++ bboard/admin.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 
 from .models import Bd
 
-admin.site.register(Bd)
+class BdAdmib(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Bd, BdAdmib)
+
