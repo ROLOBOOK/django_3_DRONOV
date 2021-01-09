3b2ef0d3f5db27a120ad22dd33dd6f45a49796f0
rolobook <rolobook@yandex.ru>
2021-01-09 20:22:31 +0300

    add rubric in admin of site

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Bd
+from .models import Rubric
+
 
 class BdAdmib(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
 
 admin.site.register(Bd, BdAdmib)
-
+admin.site.register(Rubric)
