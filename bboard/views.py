d1d0f40ab21b09de95a7f9725041d7ae84973a47
rolobook <rolobook@yandex.ru>
2021-01-10 20:01:06 +0300

    add create.html

--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
 
 from .models import Bd
 from .models import Rubric
-
+from .forms import BdForm
 
 # Create your views here.
 def index(request):
@@ -18,3 +18,13 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+class BdCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BdForm
+    success_url = '/bboard'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
